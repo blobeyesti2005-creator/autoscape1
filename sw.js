80f5652db0c6052de1a9d1918e51e13b545d352f
blobeyesti2005-creator <blobeyesti2005@gmail.com>
2026-08-10 19:05:33 -0700

    Cache AutoScape for offline launches

---
+++ sw.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const CACHE_VERSION = 'autoscape-v2.4';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_VERSION)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys
+          .filter(key => key.startsWith('autoscape-') && key !== CACHE_VERSION)
+          .map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+async function cacheFirst(request) {
+  const cached = await caches.match(request);
+  if (cached) return cached;
+
+  const response = await fetch(request);
+  if (request.method === 'GET' && (response.ok || response.type === 'opaque')) {
+    const cache = await caches.open(CACHE_VERSION);
+    await cache.put(request, response.clone());
+  }
+  return response;
+}
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(async response => {
+          if (response.ok) {
+            const cache = await caches.open(CACHE_VERSION);
+            await cache.put('./index.html', response.clone());
+          }
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(cacheFirst(event.request));
+});
