e6f50d2c0788592da5a7c653c05ae08d5256334a
blobeyesti2005-creator <blobeyesti2005@gmail.com>
2026-08-10 19:13:59 -0700

    Bump offline cache to v2.5

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_VERSION = 'autoscape-v2.4';
+const CACHE_VERSION = 'autoscape-v2.5';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
